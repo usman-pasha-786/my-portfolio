5858fc3a9d333c14140ec8008f1b2c151a12857d
usman-pasha-786 <147035343+usman-pasha-786@users.noreply.github.com>
2024-04-21 18:34:24 +0500

    add some projects

--- src/app/components/project.tsx
+++ src/app/components/project.tsx
@@ -15,77 +15,210 @@ const Project = () => {
     </div>
     <div className="flex flex-wrap -m-4 -mt-[5rem]">
       {/* project 1  */}
-      <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
+      <div className="lg:w-1/3 sm:w-1/ p-4 cursor-pointer">
         <div className="flex relative">
           <Image
-            alt="gallery"
+            alt="calculator"
             className="absolute inset-0 w-full h-full object-cover object-center"
-            src={require("../../../public/assets/projects/project1.jpg")}
+            src={require("../../../public/assets/projects/calculator1.png")}
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
-              project name
+              Project....
             </h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              facebook clone project
+              Mini Calculator
             </h1>
             <p className="leading-relaxed line-clamp-2">
-              there is project detail . here you will mention the detail
+            I have created this project by using typescript.
             </p>
-              <Link target='_blank' href={"#"}>
-            <p className="leading-relaxed text-blue-500 hover:underline">View project.....</p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/calculator.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">View project on Github</p>
             </Link>
           </div>
         </div>
+       
       </div>
+
+
        {/* project 2  */}
        <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
         <div className="flex relative">
           <Image
             alt="gallery"
             className="absolute inset-0 w-full h-full object-cover object-center"
-            src={require("../../../public/assets/projects/project2.jpg")}
+            src={require("../../../public/assets/projects/ngg.png")}
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
-              project name
+              Project....
             </h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              fake project
+              Number Guessing Game
             </h1>
             <p className="leading-relaxed line-clamp-2">
-              there is project detail . here you will mention the detail
+            I have created this project by using typescript.
             </p>
-              <Link target='_blank' href={"#"}>
-            <p className="leading-relaxed text-blue-500 hover:underline">View project.....</p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/number_guessing_game.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">
+              View project on Github</p>
             </Link>
           </div>
         </div>
       </div>
+
        {/* project 3  */}
        <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
         <div className="flex relative">
           <Image
             alt="gallery"
             className="absolute inset-0 w-full h-full object-cover object-center"
-            src={require("../../../public/assets/projects/project3.jpg")}
+            src={require("../../../public/assets/projects/atm.png")}
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
-              project name
+            Project....
             </h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-              fake project
+              ATM
             </h1>
             <p className="leading-relaxed line-clamp-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aspernatur, a minus aliquid eius quis incidunt eligendi, porro provident accusantium, cum harum consectetur tenetur tempore voluptas! Perspiciatis, culpa consequatur? Similique.
+            I have created this project by using typescript. 
             </p>
-              <Link target='_blank' href={"#"}>
-            <p className="leading-relaxed text-blue-500 hover:underline">View project.....</p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/atm.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">
+            View project on Github</p>
             </Link>
           </div>
         </div>
       </div>
+
+      {/* project 4  */}
+      <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
+        <div className="flex relative">
+          <Image
+            alt="gallery"
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src={require("../../../public/assets/projects/to-do.png")}
+          />
+          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+            <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
+            Project....
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              To-do List
+            </h1>
+            <p className="leading-relaxed line-clamp-2">
+            I have created this project by using typescript. 
+            </p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/todo-list.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">
+            View project on Github</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    {/* project 5 */}
+    <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
+        <div className="flex relative">
+          <Image
+            alt="gallery"
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src={require("../../../public/assets/projects/currency.png")}
+          />
+          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+            <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
+            Project....
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              ATM
+            </h1>
+            <p className="leading-relaxed line-clamp-2">
+            I have created this project by useing typescript. 
+            </p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/currency_converter.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">
+            View project on Github</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* project 6  */}
+      <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
+        <div className="flex relative">
+          <Image
+            alt="gallery"
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src={require("../../../public/assets/projects/ag.png")}
+          />
+          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+            <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
+            Project....
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              Adventure Game
+            </h1>
+            <p className="leading-relaxed line-clamp-2">
+            I have created this project by using typescript. 
+            </p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/project07-adventure-game.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">
+            View project on Github</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* project 7  */}
+      <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
+        <div className="flex relative">
+          <Image
+            alt="gallery"
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src={require("../../../public/assets/projects/bank.png")}
+          />
+          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+            <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
+            Project....
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              ATM
+            </h1>
+            <p className="leading-relaxed line-clamp-2">
+            I have created this project by using typescript. 
+            </p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/my-bank.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">
+            View project on Github</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* project 8 */}
+      <div className="lg:w-1/3 sm:w-1/3 p-4 cursor-pointer">
+        <div className="flex relative">
+          <Image
+            alt="gallery"
+            className="absolute inset-0 w-full h-full object-cover object-center"
+            src={require("../../../public/assets/projects/sms.png")}
+          />
+          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
+            <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
+            Project....
+            </h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+              Student Manangement System
+            </h1>
+            <p className="leading-relaxed line-clamp-2">
+            I have created this project by using typescript. 
+            </p>
+              <Link target='_blank' href={"https://github.com/usman-pasha-786/student-management-system.git"}>
+            <p className="leading-relaxed text-blue-500 hover:underline">
+            View project on Github</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+     
 
     </div>
   </div>
